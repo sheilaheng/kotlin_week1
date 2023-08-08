ebb702cbdd4134a43e354816eeda46f597404c2c
allanKarige <allankarige@gmail.com>
2023-08-08 09:32:19 +0300

    KOTLIN

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+  //variables in kotlin
+    var name = "Kim"
+    var age = 78
+    println(name)
+    println(age)
+
+
+    var city:String = "Nairobi"
+    var idnum:Int = 21456789
+    println(city)
+    println(idnum)
+
+}
---
+++ src/main/kotlin/varfile.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    var num1 = 20
+    var num2 = 78
+
+
+    var result = num1 + num2
+    println(result)
+
+    var num3 = 700
+    var num4 = 20
+
+    var ans = num3 / num4
+    println(ans)
+
+    var num5 = 50
+    var num6 = 40
+
+    var answer = num5 * num6
+    println(answer)
+
+
+
+
+
+
+
+
+}
