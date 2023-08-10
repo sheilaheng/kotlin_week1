621926bcdeb04a165960d5d3cefb3ff78dbe7b2f
allanKarige <kwambokasheilah45@gmail.com>
2023-08-10 10:10:03 +0300

    age calculator

--- src/main/kotlin/simpleex.kt
+++ src/main/kotlin/simpleex.kt
@@ -4,14 +4,11 @@ fun main(args: Array<String>) {
     //write a simple kotlin program enter year born and it displays your age
 
     println("Enter your year of birth")
-    var birthyear:Int = readLine()!!.toInt()
+    var birthyear: Int = readLine()!!.toInt()
 
     var currentyear = Calendar.getInstance().get(Calendar.YEAR)
     var age = currentyear - birthyear
+
     println("You are $age years old")
-
-
-
-
 
 }
