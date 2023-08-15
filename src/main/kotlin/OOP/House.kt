3a23c64e7ad117c7349cb85d41cafd1a4266ff49
allanKarige <kwambokasheilah45@gmail.com>
2023-08-15 08:44:53 +0300

    OOP

--- src/main/kotlin/OOP/House.kt
+++ src/main/kotlin/OOP/House.kt
@@ -1,14 +1,14 @@
 package OOP
 
-class House(owner:String,location:String,price:Int) {
+class House(type:String,owner:String,location:String,price:Int) {
 
     init {
-        println("House owned by  $owner,is located in $location worth $price")
+        println("$type owned by  $owner is located in $location worth $price")
     }
 
 }
 
 fun main(args: Array<String>) {
-    var owner1 = House("Tim","Kilimani",5300000)
-    var owner2 = House("Barbara","Ngong",2300000)
+    var owner1 = House("Mansion","Tim","Kilimani",5300000)
+    var owner2 = House("Apartments","Barbara","Ngong",2300000)
 }
